0efcfd4f7e9c741e852f0a24db1dfba3336b2943
Daniel Baker <daniel.baker@tweag.io>
2023-07-10 01:02:50 -0700

    got devshell, actions, and workflows setup.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Southern California Nix User Group Site";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.npmlock2nix.url = "github:nix-community/npmlock2nix";
+  inputs.npmlock2nix.flake = false;
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    npmlock2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              npmlock2nix = pkgs.callPackage npmlock2nix {};
+            })
+          ];
+        };
+
+        eventShell = pkgs.npmlock2nix.v2.shell {
+          src = ./.;
+          nodejs = pkgs.nodejs;
+          # node_modules_mode = "copy";
+        };
+      in {
+        devShells.eventShell = eventShell;
+      }
+    );
+}
