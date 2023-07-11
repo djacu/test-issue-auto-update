823c9a93b57e1b57e3d191eae4da2dc961bb8f4e
Daniel Baker <daniel.baker@tweag.io>
2023-07-10 22:56:17 -0700

    debugging

--- .github/actions/update-upcoming-events/script.js
+++ .github/actions/update-upcoming-events/script.js
@@ -24,9 +24,9 @@ run();
 async function run() {
     // Create Octokit constructor with custom user agent
     const octokit = new Octokit({
-        // auth: secrets.PERSONAL_ACCESS_TOKEN,
-        auth: process.env.GITHUB_TOKEN,
-        userAgent: "djacu",
+        auth: secrets.PERSONAL_ACCESS_TOKEN,
+        // auth: process.env.GITHUB_TOKEN,
+        // userAgent: "djacu",
     });
 
     // load all open issues with the `event` label
